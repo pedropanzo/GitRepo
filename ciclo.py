f500601f587f6da52e9b8b3cdaacd5fa480b98b3
Pedro Panzo <pedropanzo53@gmail.com>
2019-01-03 22:13:12 -0300

    Update automático - 03/01/2019 22:13:12

--- ciclo.py
+++ ciclo.py
@@ -16,7 +16,7 @@ from datetime import datetime
 # CONFIGURAÇÃO
 # ──────────────────────────────────────────────
 REP_MIN             = 1  # mínimo de repetições por ciclo
-REP_MAX             = 20  # máximo de repetições por ciclo
+REP_MAX             = 5  # máximo de repetições por ciclo
 PAUSA_ENTRE_SCRIPTS = 2   # 3 segundos entre tex.py e com.py
 PAUSA_ENTRE_CICLOS  = 5  # 10 segundos entre cada ciclo completo
 MAX_CICLOS          = 0   # 0 = infinito
